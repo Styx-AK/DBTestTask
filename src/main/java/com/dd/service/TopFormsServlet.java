9abfe2f258d19407398065f94f680a4073c46ba8
DenisFromWork <dlukashov@akolchin.com>
2018-09-19 19:06:50 +0300

    -complete top 5 form report

--- src/main/java/com/dd/service/TopFormsServlet.java
+++ src/main/java/com/dd/service/TopFormsServlet.java
@@ -2,24 +2,61 @@ package com.dd.service;
 
 import com.dd.utils.JDBCUtil;
 
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.dd.utils.JDBCUtil.close;
+
 
 public class TopFormsServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        resp.getWriter().write("В) Составить ТОП – 5 самых используемых форм.");
+        resp.getWriter().write("<h4>В) Составить ТОП – 5 самых используемых форм.</h4>");
 
         JDBCUtil jdbcUtil = new JDBCUtil();
 
         Statement statement = null;
         ResultSet resultSet = null;
+        String sql = "SELECT DISTINCT formid FROM test_task_table ORDER BY formid DESC LIMIT 6;";
+        //LIMIT 6 - because the first row is null(empty) and this row won't be included into "formids".
+
+        List<String> formids = new ArrayList<>();
+
+        try {
+            System.out.println("Creating connection to database...");
+            jdbcUtil.getMyConnection();
+            statement = jdbcUtil.getMyConnection().createStatement();
+
+            System.out.println("Making query request...");
+            resultSet = statement.executeQuery(sql);
+
+            while(resultSet.next()) {
+                String formid = resultSet.getString("formid");
+                if (formid != null) {
+                    formids.add(formid);
+                }
+            }
+
+            close(resultSet, statement, jdbcUtil.getMyConnection());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            resp.getWriter().println("<h4>Ops! Something went wrong...</h4>");
+        }
+
+        req.setAttribute("formids", formids);
+
+        req.getRequestDispatcher("WEB-INF/view/topforms.jsp").forward(req, resp);
     }
 }
--- src/main/webapp/WEB-INF/view/topforms.jsp
+++ src/main/webapp/WEB-INF/view/topforms.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
          pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -9,9 +10,15 @@
 </head>
 
 <body>
-<p>Create table and import data from CSV file to that table</p>
-<a href="/cc">Do it!</a><br>
-<p>If the table have been already created and filled, go and get reports you need</p>
-<a href="/reports">Links to Reports</a><br>
+<table>
+    <tr>
+        <th>id</th>
+    </tr>
+    <c:forEach items="${formids}" var="id">
+        <tr>
+            <td>${id}</td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
